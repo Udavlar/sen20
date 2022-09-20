29716178a850cfd2046079eb51da4be466f1419d
Larisa <udavlar@mail.ru>
2022-09-20 14:52:06 +0500

    p1

---
+++ rab1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("Введите первое число: ");
+int num1 = int.Parse (Console.ReadLine());
+Console.WriteLine("Ведите второе число: ");
+int num2 = int.Parse (Console.ReadLine());
+if (num1% num2 == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine($"Не кратно, остаток- {num1 % num2}");
+}
