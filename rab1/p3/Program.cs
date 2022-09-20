58adfe66aacea03af4e3f26643a2689b545dde2b
Larisa <udavlar@mail.ru>
2022-09-20 17:28:50 +0500

    lчастное от 7 и 23

---
+++ rab1/p3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число ");
+int num = int.Parse (Console.ReadLine());
+if ((num % 7 == 0)&&( num % 23 ==0))
+{
+    Console.WriteLine("Да");
+}else{
+    Console.WriteLine("Нет");
+}
